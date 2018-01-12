82c32df8eaf0eeb5e82bd106cdcf4611c294b0da
feihafferkamp <fei.hafferkamp@gmail.com>
2018-01-11 22:39:13 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+require 'pry'
+# require_relative './artist'
+
+class Song
+  attr_accessor :title, :artist
+
+  # def initialize (title, artist)
+  #   @title = title
+  #   @artist = Artist.new
+  # end
+end
