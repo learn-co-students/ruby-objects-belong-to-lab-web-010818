aa4af32bde86c06b834c3f93b87dfd80496fb737
JoshD1 <joshdeiner75@gmail.com>
2018-01-11 20:12:40 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post
+  attr_accessor :title, :author
+  def initializer(title)
+    @title = title
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+
+  attr_accessor :title, :artist
+  def initializer(title)
+    @title = title
+  end
+
+
+end
